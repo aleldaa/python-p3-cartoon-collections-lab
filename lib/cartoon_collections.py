20f7ebb1ad18f75282c7dc7f5e798b38a5227e24
Luna Daniels <aleldaproductions@gmail.com>
2023-05-03 15:21:47 -0600

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_names):
+    for i, name in enumerate(dwarf_names):
+        print(f'{i+1}. {name}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f'{call.title()}!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
